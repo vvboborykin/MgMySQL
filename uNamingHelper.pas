649e11dab771c09c486a477f851924868eabd36f
bpost@yandex.ru <vvboborykin>
2021-12-28 07:30:53 +0300

    Update uNamingHelper.pas
    Reserved words

--- uNamingHelper.pas
+++ uNamingHelper.pas
@@ -11,7 +11,8 @@ unit uNamingHelper;
 interface
 
 uses
-  System.Classes, System.SysUtils, System.Rtti, System.RegularExpressions;
+  System.Classes, System.SysUtils, System.Rtti, System.StrUtils,
+  System.RegularExpressions;
 
 type
   TNamingHelper = class
@@ -19,10 +20,14 @@ type
     FTablePrefix: string;
     FReservedWords: TArray<string>;
     procedure AppendClassMembersToReservedWords(AClass: TClass; ARtti:
-        TRttiContext);
+      TRttiContext);
+    procedure AppendClassNamesToReservedWords(AClass: TClass);
+    function IsReservedWord(AText: string): Boolean;
+    function ToNotReservedWord(AText: string): string;
     procedure RegisterReservedWords(AExcludeMembersOf: array of TClass);
     function ReplaceInvalidCharacters(ATableName: string): string;
     procedure SetTablePrefix(const Value: string);
+    function ToUpperFirstChar(AText: string): string;
   private
   public
     constructor Create(const APrefix: string; AExcludeMembersOf: array of TClass);
@@ -35,16 +40,43 @@ type
 
 implementation
 
-constructor TNamingHelper.Create(const APrefix: string; AExcludeMembersOf: array of TClass);
+const
+  cDelphiLanguageReservedWords: array of string = ['and', 'end', 'interface',
+    'record', 'var', 'array', 'except', 'is', 'repeat', 'while', 'as', 'exports',
+    'label', 'resourcestring', 'with', 'asm', 'file', 'library', 'set', 'xor',
+    'begin', 'finalization   ', 'mod', 'shl', 'case', 'finally', 'nil', 'shr',
+    'class', 'for', 'not', 'string', 'const', 'function', 'object', 'then',
+    'constructor', 'goto', 'of', 'threadvar', 'destructor', 'if', 'or', 'to',
+    'dispinterface     ', 'implementation    ', 'packed', 'try', 'div', 'in',
+    'procedure', 'type', 'do', 'inherited', 'program', 'unit', 'downto',
+    'initialization', 'property', 'until', 'else', 'inline', 'raise', 'uses',
+    'absolute', 'export12', 'name', 'public', 'stdcall', 'abstract', 'external',
+    'near1', 'published', 'strict', 'assembler', 'far', 'nodefault', 'read',
+    'stored', 'automated', 'final', 'operator', 'readonly', 'unsafe', 'cdecl',
+    'forward', 'out', 'reference', 'varargs', 'contains', 'helper', 'overload',
+    'register', 'virtual', 'default', 'implements', 'override', 'reintroduce',
+    'winapi', 'delayed', 'index', 'package', 'requires', 'write', 'deprecated11',
+    'inline', 'pascal', 'resident1', 'writeonly', 'dispid', 'library',
+    'platform11', 'safecall', 'dynamic', 'local', 'private', 'sealed',
+    'experimental11', 'message', 'protected', 'static'];
+  cDelphiScalarDataTypes: array of string = ['string', 'integer', 'boolean',
+    'decimal', 'float', 'double', 'longint', 'int64', 'short', 'byte', 'variant',
+    'tvarrec', 'olevariant', 'widestring', 'wchar', 'char'];
+
+constructor TNamingHelper.Create(const APrefix: string; AExcludeMembersOf: array
+  of TClass);
 begin
   inherited Create;
   FTablePrefix := APrefix;
+  FReservedWords := nil;
   RegisterReservedWords(AExcludeMembersOf);
 end;
 
-procedure TNamingHelper.AppendClassMembersToReservedWords(AClass: TClass;
-    ARtti: TRttiContext);
+procedure TNamingHelper.AppendClassMembersToReservedWords(AClass: TClass; ARtti:
+  TRttiContext);
 begin
+  AppendClassNamesToReservedWords(AClass);
+
   var vInstanceRtti := TRttiInstanceType(ARtti.GetType(AClass));
 
   for var vPropInfo in vInstanceRtti.GetProperties() do
@@ -52,6 +84,27 @@ begin
 
   for var vMethodInfo in vInstanceRtti.GetMethods() do
     FReservedWords := FReservedWords + [vMethodInfo.Name];
+end;
+
+procedure TNamingHelper.AppendClassNamesToReservedWords(AClass: TClass);
+begin
+  FReservedWords := FReservedWords + [AClass.ClassName];
+  if AClass <> TObject then
+    AppendClassNamesToReservedWords(AClass.ClassParent);
+end;
+
+function TNamingHelper.IsReservedWord(AText: string): Boolean;
+begin
+  Result := (AnsiIndexText(AText, FReservedWords) >= 0) or (AnsiIndexText(AText,
+    cDelphiLanguageReservedWords) >= 0) or (AnsiIndexText(AText,
+    cDelphiScalarDataTypes) >= 0);
+end;
+
+function TNamingHelper.ToNotReservedWord(AText: string): string;
+begin
+  Result := AText;
+  if IsReservedWord(AText) then
+    AText := AText + '_';
 end;
 
 procedure TNamingHelper.RegisterReservedWords(AExcludeMembersOf: array of TClass);
@@ -89,7 +142,16 @@ end;
 
 function TNamingHelper.TableNameToObjectName(ATableName: string): string;
 begin
-  Result := FTablePrefix + ReplaceInvalidCharacters(ATableName);
+  Result := ToUpperFirstChar(ToNotReservedWord(FTablePrefix +
+    ReplaceInvalidCharacters(ATableName)));
+end;
+
+function TNamingHelper.ToUpperFirstChar(AText: string): string;
+begin
+  if AText = '' then
+    Result := ''
+  else
+    Result := AnsiUpperCase(AText[1]) + RightStr(AText, Length(AText) - 1);
 end;
 
 end.
